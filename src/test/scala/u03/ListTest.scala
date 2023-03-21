6f9ea5e0c2d906e2400eda608611f4df0da009b8
Leo_Marzoli <leo.marzoli@studio.unibo.it>
2023-03-21 20:59:58 +0100

    Risolto dei bug relativi alla libreria jdk scala che non veniva riconosciuta riavviando la cache di IntelliJ. Impostato il primo test di "drop".

--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -20,3 +20,9 @@ class ListTest:
   @Test def testFilter() =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_>=20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_!=20))
+
+  @Test
+  def testDropList() =
+    assertEquals(Cons(20, Cons(30, Nil())), drop(lst, 1)) // Cons (20 , Cons (30 , Nil ()))
+    assertEquals(Cons(30, Nil()), drop(lst, 2)) // Cons (30 , Nil ())
+    assertEquals(Nil(), drop(lst, 5)) // Nil ()
